d73dbcdcb38c2baf170c45c0e3263ea90a41a6b9
Roger O'Dell <odell.instructor@gmail.com>
2024-02-15 16:17:36 -0600

    Lesson 3 - My application overview

---
+++ JavaFundamentals/src/main/java/com/fundamentals/main/House.java
@@ -0,0 +1,13 @@
+package com.fundamentals.main;
+
+public class House {
+    public String entranceDoorColor;
+    public String foundationType;
+    public String roofStyle;
+    public int totalWindows;
+
+    public void doorFunction() {
+        System.out.println("This door opens.");
+    }
+
+}
--- JavaFundamentals/src/main/java/com/fundamentals/main/Main.java
+++ JavaFundamentals/src/main/java/com/fundamentals/main/Main.java
@@ -4,6 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Java");
+        houseExample();
+    }
+
+    // Single Comment
+    /* Multi-line comment
+    This is a second line
+    And a third line.
+    * */
+
+    public static void houseExample() {
+        House myHouse = new House();
+        myHouse.doorFunction();
+        myHouse.entranceDoorColor = "Red";
+        myHouse.foundationType = "Concrete";
+        myHouse.roofStyle = "Cedar Shingle";
+        myHouse.totalWindows = 30;
+        System.out.println(myHouse.entranceDoorColor);
     }
 
 }
