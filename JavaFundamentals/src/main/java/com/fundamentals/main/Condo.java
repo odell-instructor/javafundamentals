32e16345d92606882d70e5dc3b4af6cc90aaa259
Roger O'Dell <odell.instructor@gmail.com>
2024-03-07 15:27:34 -0600

    House and condo updates

---
+++ JavaFundamentals/src/main/java/com/fundamentals/main/Condo.java
@@ -0,0 +1,34 @@
+package com.fundamentals.main;
+
+/* Child of the House class */
+public class Condo extends House {
+    private String balconyType;
+
+    public Condo(String balconyType) {
+        this("Concrete", "Red", "Flat", 87, balconyType);
+    }
+
+    public Condo(String foundationType, String doorColor,
+                 String roofStyle, int totalWindows, String balconyType) {
+        this.balconyType = balconyType;
+        this.foundationType = foundationType;
+        this.entranceDoorColor = doorColor;
+        this.roofStyle = roofStyle;
+        this.totalWindows = totalWindows;
+    }
+
+    public String getBalconyType() {
+        return balconyType;
+    }
+
+    public void maintenance() {
+        System.out.println("Maintenance has been called.");
+    }
+
+    public static void main(String[] args) {
+        Condo myCondo = new Condo("Railed");
+        System.out.println(myCondo.getEntranceDoorColor());
+        System.out.println(myCondo.getFoundationType());
+    }
+
+} // end class
--- JavaFundamentals/src/main/java/com/fundamentals/main/House.java
+++ JavaFundamentals/src/main/java/com/fundamentals/main/House.java
@@ -1,10 +1,10 @@
 package com.fundamentals.main;
 
 public class House {
-    private String entranceDoorColor;
-    private String foundationType;
-    private String roofStyle;
-    private int totalWindows;
+    protected String entranceDoorColor;
+    protected String foundationType;
+    protected String roofStyle;
+    protected int totalWindows;
 
     public House() {
         this("Cinder Blocks", "Grey");
@@ -42,8 +42,21 @@ public class House {
         System.out.println("This door opens.");
     }
 
-    public static void main(String[] args) {
+    /* Overloaded method - Can only happen when
+    * the method signature is different between the
+    * defined method by name. The signature of a
+    * method is the name + the data types that represent
+    * the parameter argument. The return type is not
+    * part of the signature for overloading.  */
+    public void doorFunction(String doorColor) {
+        System.out.println("This " + doorColor +
+                " door opens");
+    }
 
+    public static void main(String[] args) {
+        House myHouse = new House();
+        myHouse.doorFunction();
+        myHouse.doorFunction(myHouse.getEntranceDoorColor());
     }
 
 }
